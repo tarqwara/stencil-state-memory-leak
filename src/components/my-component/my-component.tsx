aee6f8a8f707a9da3723aded857dea31858c34bc
Erki Kütt <erki.kytt@elisa.ee>
2021-11-30 10:54:36 +0200

    initial commit

---
+++ src/components/my-component/my-component.tsx
@@ -0,0 +1,38 @@
+import { Component, Prop, State, h } from '@stencil/core';
+import { format } from '../../utils/utils';
+
+@Component({
+  tag: 'my-component',
+  styleUrl: 'my-component.css',
+  shadow: true,
+})
+export class MyComponent {
+  @State() stateVar: boolean = false;
+
+  /**
+   * The first name
+   */
+  @Prop() first: string;
+
+  /**
+   * The middle name
+   */
+  @Prop() middle: string;
+
+  /**
+   * The last name
+   */
+  @Prop() last: string;
+
+  componentDidLoad() {
+    this.stateVar = true;
+  }
+
+  private getText(): string {
+    return format(this.first, this.middle, this.last);
+  }
+
+  render() {
+    return <div>Hello, World! I'm {this.getText()}</div>;
+  }
+}
